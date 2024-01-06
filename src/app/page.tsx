6e35101777efd7d6fa4fd9d32b201afab363822c
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-01-06 20:58:10 +0530

    Add simple landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-import Image from 'next/image'
+import LoginButton from "@/components/auth/login-btn";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const poppinsFont = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <p className="font-semibold">Hello Auth</p>
-  )
+    <main className="flex h-full flex-col items-center justify-center bg-custom-gradient">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            poppinsFont.className
+          )}
+        >
+          🔐Auth
+        </h1>
+        <p className="text-white text-lg">Simple Auth Service </p>
+        <div className="">
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/components/auth/login-btn.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+type LoginButtonProps = {
+  children?: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+};
+
+const LoginButton = ({
+  asChild,
+  children,
+  mode = "redirect",
+}: LoginButtonProps) => {
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Login Button Clicked");
+    router.push("/auth/login");
+  };
+
+  if(mode === "modal") {
+    return (
+      <span>
+        TODO: Implement Modal
+      </span>
+    )
+  }
+
+  return (
+    <span onClick={handleClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
+
+export default LoginButton;
