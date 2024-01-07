a92796115a23d8973b6a488f44fc5994c29bf76c
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-01-08 00:52:06 +0530

    Add callbacks

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,16 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import db from "@/lib/db";
+import db, { UserRole } from "@/lib/db";
+import { getUserById } from "./data/user";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: UserRole;
+    } & DefaultSession["user"];
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -14,8 +23,33 @@ export const {
   ...authConfig,
   callbacks: {
     jwt: async ({ token }) => {
-      console.log(token);
+      if (!token.sub) return token;
+
+      const user = await getUserById(token.sub);
+      if (!user) return token;
+
+      token.role = user.role;
       return token;
     },
+    session: async ({ session, token }) => {
+      console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    // signIn: async ({ user }) => {
+    //   const existingUser = await getUserById(user.id);
+      
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+      
+    //   return true;
+    // }
   },
 });
